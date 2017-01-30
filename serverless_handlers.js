6552724e3bd6c4be3c091bbc6b6dd6a529c006fc
Clement Prevost <clement.prevost@inovia-team.com>
2017-01-30 19:13:07 +0100

    add bonjour slack command

--- serverless_handlers.js
+++ serverless_handlers.js
@@ -2,4 +2,5 @@ const slack = require('./src/helper/slack');
 
 module.exports.hello =  slack.serverlessHandler(require('./src/hello'));
 module.exports.parrot =  slack.serverlessHandler(require('./src/parrot'));
+module.exports.bonjour =  slack.serverlessHandler(require('./src/bonjour'));
 
